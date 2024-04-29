7311c26021b10ebe436a3dcbe738326b211f0236
Soma Kurisu <0xe2836cb4@gmail.com>
2024-04-29 11:09:13 +0200

    metric for exceptions

--- hello-prometheus/simple-client.py
+++ hello-prometheus/simple-client.py
@@ -1,12 +1,17 @@
 import http.server
+import random
 from prometheus_client import start_http_server
 from prometheus_client import Counter
 
 REQUESTS = Counter('hello_worlds_total', 'Hello Worlds requested.')
+EXCEPTIONS = Counter('hello_world_exceptions_total', 'Exceptions serving Hello World.')
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         REQUESTS.inc()
+        with EXCEPTIONS.count_exceptions():
+            if random.random() < 0.2:
+                raise Exception
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hello World")
